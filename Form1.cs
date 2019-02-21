d352aeea5455b45630d1d6c8fbdaca042de46014
Yago Covelo <yago.covelo@hotmail.es>
2019-02-21 13:35:13 +0100

    init

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point point = new Point();
+
+            bool result = WindowsAPI.GetCursorPos(ref point);
+
+            int x = point.X;
+            int y = point.Y;
+            while (true)
+            {
+                WindowsAPI.GetCursorPos(ref point);
+                if (x != point.X)
+                {
+                    Form2 m = new Form2();
+                    m.Show();
+                    break;
+                }
+            }
+
+        }
+
+        public class WindowsAPI
+        {
+            [DllImport("user32.dll")]
+            public static extern bool GetCursorPos(ref Point pt);
+        }
+    }
+
+}
---
+++ Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(wmp_PlayStateChange);
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            axWindowsMediaPlayer1.URL = @"ruta video";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+
+        
+
+        private void wmp_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 8)
+            {
+                System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+            }
+        }
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
